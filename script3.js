2ce76db3cae3bd122eb3232c9b7002612c56c00e
Aggarwalg01 <123935463+Aggarwalg01@users.noreply.github.com>
2023-05-24 06:18:38 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,25 @@
+// Define a function to handle user input and generate bot responses
+function chatBot(input) {
+    // Convert user input to lowercase for easier handling
+    var userInput = input.toLowerCase();
+  
+    // Bot's predefined responses
+    var botResponses = {
+      "hello": "Hello! How can I assist you today?",
+      "how are you": "I'm a bot, so I don't have feelings, but thanks for asking!",
+      "goodbye": "Goodbye! Have a great day!",
+      "default": "I'm sorry, I didn't understand that. Can you please rephrase?"
+    };
+  
+    // Check if the user input matches any predefined responses
+    if (botResponses[userInput]) {
+      return botResponses[userInput];
+    } else {
+      return botResponses["default"];
+    }
+  }
+  
+  // Example usage:
+  var userInput = prompt("Ask me a question or say something:");
+  var botResponse = chatBot(userInput);
+  console.log(botResponse);
